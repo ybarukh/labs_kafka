29be5a705c58a6620edd9290fc330dda3770ec01
barukh <ybarukh@gmail.com>
2025-09-18 13:55:36 +0200

    producer/consumer

--- consumer/src/main/java/org/example/ConsumerApp.java
+++ consumer/src/main/java/org/example/ConsumerApp.java
@@ -4,8 +4,11 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -16,13 +19,15 @@ public class ConsumerApp {
         String topicName = "vehicle-count";
         String groupId = "iot-consumer-group";
         Properties configs = new Properties();
-        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, );
-        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, );
-        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, );
-        configs.put(ConsumerConfig.GROUP_ID_CONFIG, );
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 5000000);
+        configs.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 5000);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 
-        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>()) {
-            kafkaConsumer.subscribe();
+        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(configs)) {
+            kafkaConsumer.subscribe(List.of(topicName));
 
             while (true){
                 ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
--- producer/src/main/java/org/example/ProducerApp.java
+++ producer/src/main/java/org/example/ProducerApp.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 import java.util.Random;
@@ -21,18 +22,23 @@ public class ProducerApp {
         final String topicName = "vehicle-count";
 
         final Properties configs = new Properties();
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, );
-        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, );
-        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, );
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.BATCH_SIZE_CONFIG, 1024 * 10);
+        configs.put(ProducerConfig.LINGER_MS_CONFIG, 5000);
+        configs.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
-        try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>()) {
+        int counter = 0;
+        try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(configs)) {
             for (int i = 0; i < NUMBER_OF_RECORD; i++) {
                 String key = "sensor-" + i;
-                int value = new Random().nextInt(MAX - MIN) + MIN;;
+//                int value = new Random().nextInt(MAX - MIN) + MIN;
+                int value = ++counter;
 
-                ProducerRecord<String, String> producerRecord = new ProducerRecord<>();
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, key, "" +value);
                 System.out.println("Produced message: (" + key + ", " + value + ")");
-                kafkaProducer.send();
+                kafkaProducer.send(producerRecord, ProducerApp::callBack);
                 Thread.sleep(1000);
             }
         }
